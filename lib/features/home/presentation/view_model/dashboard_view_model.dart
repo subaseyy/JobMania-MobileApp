014470b4544e261551bca3246adb85e283734d9a
Subas Kandel <69797726+subasoffl@users.noreply.github.com>
2025-07-03 18:03:02 +0545

    Implement DashboardViewModel with job fetching logic

---
+++ lib/features/home/presentation/view_model/dashboard_view_model.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jobmaniaapp/features/home/domain/use_case/get_few_jobs_usecase.dart';
+
+import 'package:jobmaniaapp/features/home/presentation/view_model/dashboard_state.dart';
+
+class DashboardViewModel extends Cubit<DashboardState> {
+  final GetFewJobsUseCase getFewJobsUseCase;
+
+  DashboardViewModel({required this.getFewJobsUseCase})
+    : super(DashboardState.initial());
+
+  Future<void> loadJobs() async {
+    emit(state.copyWith(isLoading: true));
+    try {
+      final jobs = await getFewJobsUseCase();
+      print(jobs);
+      emit(state.copyWith(isLoading: false, jobs: jobs));
+    } catch (e) {
+      emit(state.copyWith(isLoading: false, error: e.toString()));
+    }
+  }
+}
