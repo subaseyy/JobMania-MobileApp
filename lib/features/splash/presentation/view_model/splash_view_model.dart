4fd7c3f3b937998bf66eb64dab2a3ed19a6b0ee8
Subas Kandel <69797726+subasoffl@users.noreply.github.com>
2025-07-03 17:18:09 +0545

    feat(splash): secure auto-login using biometric and token validation

--- lib/features/splash/presentation/view_model/splash_view_model.dart
+++ lib/features/splash/presentation/view_model/splash_view_model.dart
@@ -1,29 +1,47 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:jobmaniaapp/app/service_locator/service_locator.dart';
-import 'package:jobmaniaapp/features/auth/presentation/view/login.view.dart';
-import 'package:jobmaniaapp/features/auth/presentation/view_model/login_view_model/login_view_model.dart';
 import 'package:jobmaniaapp/features/home/presentation/view/main.view.dart';
+import 'package:jobmaniaapp/features/auth/presentation/view/login.view.dart';
+import 'package:jobmaniaapp/app/service_locator/service_locator.dart';
+import 'package:jobmaniaapp/features/auth/presentation/view_model/login_view_model/login_view_model.dart';
+import 'package:jobmaniaapp/features/splash/presentation/view_model/biometric_helper.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashViewModel extends Cubit<void> {
   SplashViewModel() : super(null);
 
   Future<void> init(BuildContext context) async {
-    await Future.delayed(const Duration(seconds: 2)); // Simulate splash delay
+    await Future.delayed(const Duration(seconds: 2));
 
     final prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString('token');
+    final token = prefs.getString('auth_token');
 
     if (context.mounted) {
       if (token != null && token.isNotEmpty) {
-        // Navigate to MainView if token exists
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (_) => const MainView()),
-        );
+        // 🔐 Biometric check
+        final auth = BiometricHelper();
+        final isAuthenticated = await auth.authenticate();
+
+        if (isAuthenticated) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => const MainView()),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Biometric authentication failed')),
+          );
+
+          // Exit the app
+          if (Platform.isAndroid) {
+            SystemNavigator.pop(); // Android
+          } else if (Platform.isIOS) {
+            exit(0); // iOS
+          }
+        }
       } else {
-        // Navigate to LoginView if no token
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
