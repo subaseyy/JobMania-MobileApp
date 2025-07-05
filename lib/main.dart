163163ad60b80c6d1f7e4006b842f589de231bfd
Subas Kandel <69797726+subasoffl@users.noreply.github.com>
2025-07-06 00:01:09 +0545

    chore: update main.dart to integrate splash and profile setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:jobmaniaapp/app/app.dart';
 import 'package:jobmaniaapp/app/service_locator/service_locator.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
