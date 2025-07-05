cb59ae8b901f2d3f7c4c8dbc794ef6f0c18847f9
Subas Kandel <69797726+subasoffl@users.noreply.github.com>
2025-07-06 00:01:42 +0545

    refactor: move navigation logic to widget via callback pattern

--- lib/features/splash/presentation/view_model/splash_view_model.dart
+++ lib/features/splash/presentation/view_model/splash_view_model.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jobmaniaapp/core/network/hive_services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
